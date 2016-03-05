d6cad0e2a2fb90ee71fe3f99d17d048f8b682b42
sergener <sibel.ergener@gmail.com>
2016-03-04 21:31:45 -0500

    Adding factorial files and solutions, adding add-it-up files 4.7

---
+++ week-4/factorial/my_solution.rb
@@ -0,0 +1,19 @@
+# Factorial
+
+# I worked on this challenge with: Kristal Lam.
+
+
+# Your Solution Below
+def factorial(number)
+ if number == 0
+   p 1
+elsif
+ i = 1
+ num = i * number
+   while number > i do
+     num = i * num
+     i += 1
+   end
+   p num
+ end
+end
