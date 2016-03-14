d55182c1bb7a3f1b56c8f13d985aa68b32f181ea
sergener <sibel.ergener@gmail.com>
2016-03-14 04:39:19 -0400

    nums-commas reflection

--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -45,3 +45,20 @@ end
 
 
 # 3. Reflection
+
+=begin
+	
+What was your process for breaking the problem down? What different approaches did you consider?
+I have a lot of trouble writing out pseudocode directly onto the screen, so this challenge was the first time I decided to write it on paper instead. This was very helpful in being able to break down the challenge. While hopefully someday I will learn to think more clearly in steps that work for Ruby, it was useful to be able to kind of freely doodle solutions in a non linear order before coming up with a final pseudocode. 
+My initial though was definitely to reverse the string and break into arrays, as I did, though I did consider some other solutions that never fully formed enough for me to implement them.
+
+Was your pseudocode effective in helping you build a successful initial solution?
+It was! I did go back at times to clarify what I needed in my pseudocode so I could reference how explicit I'd been later on.
+
+What new Ruby method(s) did you use when refactoring your solution? Describe your experience of using the Ruby documentation to implement it/them (any difficulties, etc.). Did it/they significantly change the way your code works? If so, how?
+I hadn't used .slice before. I find the docs very difficult to read so found .slice on stackoverflow to get a clearer image of what it did, then looked it up to figure out how to use it. This isn't in my refectored method, and it took so long to get to this solution that I needed a break from it.
+
+How did you initially iterate through the data structure?
+Do you feel your refactored solution is more readable than your initial solution? Why?
+	
+end
