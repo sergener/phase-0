b79b9dfc740590cae854e0037e1395241caca8ff
sergener <sibel.ergener@gmail.com>
2016-03-14 04:10:56 -0400

    Adding reflections to calculate mode

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -1,8 +1,8 @@
 # Calculate the mode Pairing Challenge
 ​
-# I worked on this challenge [by myself, with: ]
+# I worked on this challenge with: Chand Nirankari
 ​
-# I spent [] hours on this challenge.
+# I spent 4 hours on this challenge.
 ​
 # Complete each step below according to the challenge directions and
 # include it in this file. Also make sure everything that isn't code
@@ -56,3 +56,18 @@ def mode(array)
 end
 ​
 # 4. Reflection
+
+=begin
+Which data structure did you and your pair decide to implement and why?
+We decided to begin by turning the array into a hash to be able to have two necessary kinds of data: the objects in the array, and an integer representing the amount of times that object appeared. We could then use that value, the integer, to return a new array with the correct data.
+
+Were you more successful breaking this problem down into implementable pseudocode than the last with a pair? 
+It took a while to get our pseudocode to be understandable; we wrote out thoughts of how to write the code on several pages of paper before being able to narrow it down to a specific pseudocode we would try to implement. Though it was difficult and frustrating to get it right, it was also helpful in learning where we were complicating things or missing important factors.
+
+What issues/successes did you run into when translating your pseudocode to code?
+Things that were understandable pseudocode didn't always provide immediately apparent methods or solutions for us. It would probably helped to have broken it down more. For example: "Figure out what the highest value is in the hash" could be broken down into potential methods to look up.
+
+What methods did you use to iterate through the content? Did you find any good ones when you were refactoring? Were they difficult to implement?
+We found .keys, which returns keys from a hash.
+	
+=end
