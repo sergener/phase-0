8a2240d54c8c51349ecb51d1a5590fa1b2df0883
sergener <sibel.ergener@gmail.com>
2016-03-19 11:30:41 -0400

    Adding attr dir and files

---
+++ week-6/attr/my_solution.rb
@@ -0,0 +1,47 @@
+#Attr Methods
+
+# I worked on this challenge [by myself, with:]
+
+# I spent [#] hours on this challenge.
+
+# Pseudocode
+
+# Input:
+# Output:
+# Steps:
+
+class NameData
+
+end
+
+
+class Greetings
+
+end
+
+
+
+# Reflection
+=begin
+Release 1
+What are these methods doing?
+How are they modifying or returning the value of instance variables?
+
+Release 2
+What changed between the last release and this release?
+What was replaced?
+Is this code simpler than the last?
+
+Release 3
+What changed between the last release and this release?
+What was replaced?
+Is this code simpler than the last?
+
+Release 6
+What is a reader method?
+What is a writer method?
+What do the attr methods do for you?
+Should you always use an accessor to cover your bases? Why or why not?
+What is confusing to you about these methods?
+
+=end
