e1a13c86fce7b469a5c3e682bf1ef65cf6b5723c
sergener <sibel.ergener@gmail.com>
2016-03-22 10:56:52 -0400

    Slightly changing wording to group project solution user story

--- week-7/gps6/my_solution.rb
+++ week-7/gps6/my_solution.rb
@@ -6,27 +6,40 @@
 # EXPLANATION OF require_relative
 #
 #
+
+# Virus Predictor
+
+# I worked on this challenge [by myself, with: ].
+# We spent [#] hours on this challenge.
+
+# EXPLANATION OF require_relative
+#
+#
 require_relative 'state_data'
 
 class VirusPredictor
 
+  #initialize takes in three arguments and makes instance variables using the data from each argument so they can be accessed across all methods in the class
   def initialize(state_of_origin, population_density, population)
     @state = state_of_origin
     @population = population
     @population_density = population_density
   end
 
+  #virus_effects calls predicted deaths method and the speed_of_spread method
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths(@population_density)
+    speed_of_spread(@population_density)
   end
 
+  #sets subsequent methods to be private
   private
 
-  def predicted_deaths(population_density, population, state)
+  #predicted_deaths uses flow control to return number of deaths based on population density. Prints state name and number of deaths to console.
+  def predicted_deaths(population_density)
     # predicted deaths is solely based on population density
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+      number_of_deaths = (@population * 0.4).floor #returns largest integer <= float
     elsif @population_density >= 150
       number_of_deaths = (@population * 0.3).floor
     elsif @population_density >= 100
@@ -41,21 +54,21 @@ class VirusPredictor
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+  #speed_of_spread uses flow control to return how many months it will take outbreak to spread based on population density. 
+  #puts speed to finish previous method's statement.
+  def speed_of_spread(population_density) #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
-
     if @population_density >= 200
-      speed += 0.5
+      speed = 0.5
     elsif @population_density >= 150
-      speed += 1
+      speed = 1
     elsif @population_density >= 100
-      speed += 1.5
+      speed = 1.5
     elsif @population_density >= 50
-      speed += 2
+      speed = 2
     else
-      speed += 2.5
+      speed = 2.5
     end
 
     puts " and will spread across the state in #{speed} months.\n\n"
@@ -81,6 +94,18 @@ california.virus_effects
 
 alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
 alaska.virus_effects
+
+# For each key (state name) in STATE_DATA hash, initialize VirusPredictor
+# 
+
+STATE_DATA.each do |state, value|
+  vp_state = VirusPredictor.new(state, value[:population_density], value[:population])
+  vp_state.virus_effects
+end
+
+
+
+
 
 
 #=======================================================================
--- week-7/group-project/group_project_solution.js
+++ week-7/group-project/group_project_solution.js
@@ -1,6 +1,6 @@
 /*
-As a user, I want to give you a group of whole numbers to do three different math tests with.
-The tests should work whether I give you an even or odd amount of numbers to work with.
+As a user, I want to give you a group of whole numbers to do three different math problems with.
+The problems should work whether I give you an even or odd amount of numbers to work with.
 
 1. Sum: The program should be able to take those numbers and add them up.
 2. Mean: The program should also be able to take the numbers and come up with an average.
