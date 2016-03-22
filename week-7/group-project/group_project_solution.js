4e538ae88db946e8bb0989d872352043c7134b9a
sergener <sibel.ergener@gmail.com>
2016-03-22 10:53:00 -0400

    Adding group project solutions user story

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,9 @@
+/*
+As a user, I want to give you a group of whole numbers to do three different math tests with.
+The tests should work whether I give you an even or odd amount of numbers to work with.
+
+1. Sum: The program should be able to take those numbers and add them up.
+2. Mean: The program should also be able to take the numbers and come up with an average.
+3. Median: The program should be able to give a median value of all the numbers.
+
+*/
