64f75f8e5d91a85453d935248952e3a6e243c9a6
sergener <sibel.ergener@gmail.com>
2016-03-27 16:04:33 -0400

    Adding all code and steps to group_project_solution.js

--- week-7/group-project/group_project_solution.js
+++ week-7/group-project/group_project_solution.js
@@ -1,3 +1,6 @@
+//All the steps, compiled after the project was complete (all tests passed)
+
+
 /*
 As a user, I want to give you a group of whole numbers to do three different math problems with.
 The problems should work whether I give you an even or odd amount of numbers to work with.
@@ -7,6 +10,93 @@ The problems should work whether I give you an even or odd amount of numbers to 
 3. Median: The program should be able to give a median value of all the numbers.
 
 */
+
+// Pseudocode:
+
+// Input: A group of whole numbers
+// Output:
+//   A. An integer value
+//   B. A number value (either float or integer)
+//   C. A number value (either float or integer)
+
+// Steps:
+
+// A. Create a method called sum that adds up all the numbers.
+//   1. Create a counter called sum. Set it equal to 0.
+//   2. Iterate over the group of numbers. Add each number to sum.
+//   3. Return the final value of sum.
+
+// B. Create a method called mean that returns an average of all the numbers.
+//   1. Use the returned value of sum from part A.
+//   2. Count the amount of numbers in the group of numbers.
+//   3. Divide the sum by the amount of numbers.
+//   4. Return that value.
+
+// C. Create a method called median that returns the number that is in the middle of the group of numbers.
+//   1. Arrange the numbers in order from least to greatest.
+//   2. Count the amount of numbers in the group.
+//     a.IF there are an odd amount of numbers:
+//       -divide the amount of numbers by 2 to return an integer value, and add 1.
+//       -return the number that is at that index position in the group.
+//     b.ELSE (there are an even amount of numbers):
+//       -divide the amount of numbers by 2.
+//       -take a slice of 2 numbers out of the group at that index position.
+//       -add the numbers in that slice together.
+//       -divide that sum by 2.0
+//       -return that number.
+
+/*
+Initial code
+
+
+var numArray = [1,2,3]
+
+function sum(numbers) {
+  
+  var sum = 0; 
+   
+  for(var i = 0; i < numbers.length; i++) {
+    sum = sum + numbers[i];
+  }
+  return sum;
+}
+console.log(sum(numArray));
+
+
+function mean(numbers) {
+  var newSum = sum(numbers);
+  var newSumLength = numbers.length;
+  var mean = newSum / newSumLength;
+  
+  return mean;
+}
+
+console.log(mean([1,2,3]));
+
+
+function median(number) {
+  var sortedNumbers = number.sort();  
+  var countedNumbers = number.length; 
+    if(countedNumbers % 2 != 0) { 
+      var index = Math.floor((countedNumbers / 2)); 
+      return number[index];
+    }
+  else {
+    var evenIndex = (countedNumbers / 2) - 1;
+    var addedSlice = number.slice(evenIndex,evenIndex+2);
+    console.log(addedSlice);
+    var sumSlice = addedSlice[0] + addedSlice[1]; 
+    var dividedSlice = sumSlice / 2;
+    return dividedSlice;
+    } 
+}
+  
+console.log(median([1,2,3]));
+console.log(median([1,2,3,4]));
+
+
+*/
+
 
 // What I think the user stories are
 
