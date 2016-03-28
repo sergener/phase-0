00fe8e04014913b6c53aa24cbb6b197f9fa05671
sergener <sibel.ergener@gmail.com>
2016-03-27 23:57:39 -0400

    Nums_commas reflections

--- week-7/nums_commas.js
+++ week-7/nums_commas.js
@@ -44,142 +44,6 @@ Display the string
 
 // Refactored Solution
 
-//create a loop using .length to measure how long the loop should go on and keep adding commas every three positions until the end
-
-//posAdd = position + 4
-
-// var separateComma = function(number) {
-//   var num = number.toString();
-//   var arr = num.split("").reverse();
-//   // for (var position = 3; 
-//   // var position = 3
-//   var newArr = arr.splice(position, 0, ",");)
-  
-//   return arr
-/*   def separate_comma(num)
- return num.to_s if num < 1000
- num_new = num.to_s.reverse
-   array = []
-   until num_new == "" || num_new == nil
-     array << num_new.slice!(0, 3)
-   end
- array.join(",").reverse
- end
- 
- 
- def separate_comma(i)
-  if i <=3
-    return i
-  else new_i = i.to_s.reverse
-  arr = []
-    until new_i.empty?
-      arr << new_i.slice!(0..2)
-    end
-  return arr.join(",").reverse
-  end
-end
-*/
-  
-// }
-
-// console.log(separateComma(123456789123456789));
-/*
-var separateComma = function(number) {
-  
- 
-  
-  
-  
-   var num = number.toString();
-   var array = num.split("").reverse().join("");
-   var reverseNum = array.toString();
-  
-    var newArray = []
-    
-  
-  
-  
-  
-  
-   var position = reverseNum.length-3;
-   var group1 = reverseNum.substr(position,3);
-    
-   var group2 = reverseNum.substr(-6,3);
-   // var group3 = num.substr(-9,3);
-   var arrayNew = []
-   arrayNew.unshift(group1);
-   arrayNew.unshift (group2);
-  var final = arrayNew.reverse().toString();
-  return final;
-  return newArray;
-  
-};
-
-
-
-console.log(separateComma(123456789));
-*/
-/*
-var separateComma = function(number) {
-   var num = number.toString(); //number to string
-   var array = num.split("").reverse().join(""); //reverses string as array
-   var reverseNum = array.toString(); //becomes reversed string (i.e. 54321)
-  
-  //from here we want to make it into an array like this [543,21]
-
-  //I think I'm getting the syntax here wrong, but
-  //if reverseNum is longer than two numbers
-  //take three numbers from the end and put them into the array
-if (reverseNum.length >= 2;
-   var position = 2;
-   var group = reverseNum.substr(position,3);
-   var arrayNew = []
-   arrayNew.push(group);
-  else
-    arrayNew.push(reverseNum);
-  var final = arrayNew.reverse().join(",").toString();
-  return final;
-  return newArray;
-   )}
-  
-};
-*/
-
-
-
-// Your Own Tests (OPTIONAL)
-
-
-var separateComma = function(number) {
-
-
-   var stringOfNums = number.toString();
-   var splittedArray = stringOfNums.split("").reverse();
-  //  var reverseNum = array.toString();
- // return splittedArray;
-
-  //until we get to the last three numbers or less, slice and push into the new array
-  //
-  // while (splittedArray.length !== 0 )
-//   var slicedArray = splittedArray.slice(0,3);
- 
-if (splittedArray.length >= 3){
-  var slicedArray = splittedArray.slice(0,3)};
-else{
-slicedArray.push(splittedArray)
-};
-  return slicedArray;
-      // return slicedArray
-  
-
-}
-// return slicedArray;
-// console.log(slicedArray);
-console.log(separateComma(123456789));
-  
-
-
-
 
 var separateComma = function(number) {
 
@@ -208,9 +72,16 @@ var separateComma = function(number) {
 
 console.log(separateComma(123456789));
   
-
-
-
-
-
 // Reflection
+
+//What was it like to approach the problem from the perspective of JavaScript? Did you approach the problem differently?
+//We initially approached this problem differently, though in a way where we had a stronger understanding of programming languages. While we managed to talk through at least five different solutions we could have written in Ruby, it was unfortunately painfully difficult to translate those thoughts into JavaScript. We struggled with this challenge for several hours and had a lot of difficulty making it work. I think this was partially because we were so new to the language and had to do a lot of research on methods and what JavaScript is and isn't capable, but it was really hard.
+
+//What did you learn about iterating over arrays in JavaScript?
+//There aren't similar methods to iterate over an array; where I use .each very often with Ruby it wasn't available with Javascript.
+
+//What was different about solving this problem in JavaScript?
+//We had to declare a lot of variables and and create many more loops to get a working solution. 
+
+//What built-in methods did you find to incorporate in your refactored solution?
+//.splice, .join, .reverse, .length, .split were some of many methods used. They are similar to their Ruby equivilents with some differences. .reverse only applies to arrays, and you can set .splice with a array position, objects to remove, and what to add.
