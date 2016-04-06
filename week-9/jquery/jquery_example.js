7c82aac0c07fc45344d1fc8231ca418303872860
sergener <sibel.ergener@gmail.com>
2016-04-06 16:31:31 -0400

    Adding finished code and reflections to jquery challenge

--- week-9/jquery/jquery_example.js
+++ week-9/jquery/jquery_example.js
@@ -1,37 +1,45 @@
 // U3.W9:JQuery
 
 
-// I worked on this challenge [by myself, with: ].
-// This challenge took me [#] hours.
+// I worked on this challenge with: Chand.
+// This challenge took me 1 hours.
 
 $(document).ready(function(){
 
-//RELEASE 0:
-  //link the image
+$('body').css({'background-color': 'pink'});
 
-//RELEASE 1:
+//Selecting elements with jQuery (create variables)
+var $bodyElement = $('body');
+var $mascot = $(".mascot");
 
-  //Link this script and the jQuery library to the jQuery_example.html file and analyze what this code does.
+// Add code here to modify the css and html of DOM elements
+$("body h1").css({color: "blue", 
+	"border-style": "solid", 
+	visibility: "visible"});
+$(".mascot h1").html("Fiery Skippers");
 
-$('body').css({'background-color': 'pink'})
+// Modify elements with jQuery and
+// Have some fun(experiment on own) - added an animate function
 
-//RELEASE 2:
-  //Add code here to select elements of the DOM
-
-
-//RELEASE 3:
-  // Add code here to modify the css and html of DOM elements
-
-
-//RELEASE 4: Event Listener
-  // Add the code for the event listener here
-
-
-//RELEASE 5: Experiment on your own
-
-
-
-
-
-
+$('img').mouseover(function(e){
+    e.preventDefault()
+    $(this).attr('src', 'http://www.ukbutterflies.co.uk/phpBB/gallery/images/upload/f8e34e415f8e678e2992b9b8efd65243.jpg');
+    $(this).animate({
+        width: '50%',
+        opacity: '50%'
+         }, 1500 );
+  })
+$('img').mouseleave(function(){
+	$(this).attr("src", "dbc_logo.png")
+  });
 })  // end of the document.ready function: do not remove or write DOM manipulation below this.
+
+//Reflect
+//What is jQuery?
+//jQuery is a javaScript library used to manipulate HTML and CSS on websites to make them more dynamic.
+
+//What does jQuery do for you?
+//jQuery simplifies adding javaScript code and functions to websites. By using the rich library a developer is able to more easily manipulate a HTML page in ways that would be difficult with just HTML or CSS.
+
+//What did you learn about the DOM while working on this challenge?
+//By using the Console within the DOM we were able to access objects and variables and see what they were.
